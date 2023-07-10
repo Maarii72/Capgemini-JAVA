73b4de6183ad9a2fe36fd5ea1e25d5d593a70388
Marilena Sales <marilena.sales@gmail.com>
2023-07-10 19:53:07 -0300

    6G

--- Angular/prowayComputers/src/app/app-routing.module.ts
+++ Angular/prowayComputers/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NaoEncontradaComponent } from './nao-encontrada/nao-encontrada.component';
 
 //criar as rotas
 const routes: Routes = [
   { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
-  {path: "", redirectTo: "produtos", pathMatch: "full"}
+  { path: "", redirectTo: "produtos", pathMatch: "full"},
+  { path: "**", component: NaoEncontradaComponent}
 ];
 
 @NgModule({
--- Angular/prowayComputers/src/app/produtos/produtos-routing.module.ts
+++ Angular/prowayComputers/src/app/produtos/produtos-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProdutosComponent } from './produtos.component';
+import { DetalhesProdutoComponent } from './detalhes-produto/detalhes-produto.component';
 
-const routes: Routes = [{ path: '', component: ProdutosComponent }];
+const routes: Routes = [
+  { path: '', component: ProdutosComponent },
+  //quando digitar no navegado http://localhost:4200/produtos/4, vai abrir o seguinte componente
+  { path: ':id', component: DetalhesProdutoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

d8de73b93cf712ba68900418feb7be53414f8562
Marilena Sales <marilena.sales@gmail.com>
2025-05-26 20:53:05 -0300

    add

---
+++ CSS-Avancado/video/styled-components/meu-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import styled from 'styled-components';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Meu Primeiro React com Styled Components!
+        </p>
+        Seja Bem Vindo.
+      </header>
+    </div>
+  );
+}
+
+export default App;
